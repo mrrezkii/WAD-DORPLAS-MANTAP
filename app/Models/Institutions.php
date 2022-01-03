71bb4e1f95cfef37536957c1161d40eec8482931
hanstl <77946714+hanstl@users.noreply.github.com>
2022-01-03 20:55:17 +0700

    add relationship and function Institutions

--- app/Models/Institutions.php
+++ app/Models/Institutions.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Faker\Core\Blood;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +12,25 @@ class Institutions extends Model
 
     protected $table = 'institutions';
     protected $guarded = 'id_institutions';
+    protected $with = ['blood_bank', 'employees', 'donor_events', 'donor_notes', 'donor_submissions'];
+
+    public function bloodBank(){
+        $this->hasMany(BloodBank::class, 'id_blood_bank');
+    }
+
+    public function employees(){
+        $this->hasMany(Employees::class, 'id_employees');
+    }
+
+    public function donorEvents(){
+        $this->hasMany(DonorEvents::class, 'id_donor_events');
+    }
+
+    public function donorNotes(){
+        $this->hasMany(DonorNotes::class, 'id_donor_notes');
+    }
+
+    public function donorSubmissions(){
+        $this->hasMany(DonorSubmissions::class, 'id_donor_submissions');
+    }
 }
