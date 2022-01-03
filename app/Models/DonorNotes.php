f2f4eb41ce04afee9754e2765ae98995543a7360
Muhammad Ihsan Izzulhaq <mi.izzulhaq@gmail.com>
2022-01-03 21:27:11 +0700

    Make DashboardController

--- app/Models/DonorNotes.php
+++ app/Models/DonorNotes.php
@@ -11,25 +11,5 @@ class DonorNotes extends Model
 
     protected $table = 'donor_notes';
     protected $guarded = 'id_donor_notes';
-    protected $with = ['institutions', 'donators', 'donor_events', 'status_donor'];
-
-    public function institutions()
-    {
-        return $this->belongsTo(Institutions::class, 'id_institutions');
-    }
-
-    public function donators()
-    {
-        return $this->belongsTo(Donators::class, 'id_donators');
-    }
-
-    public function donorEvents()
-    {
-        return $this->belongsTo(DonorEvents::class, 'id_donor_events');
-    }
-
-    public function status()
-    {
-        return $this->belongsTo(StatusDonor::class, 'id_status_donor');
-    }
+    
 }
