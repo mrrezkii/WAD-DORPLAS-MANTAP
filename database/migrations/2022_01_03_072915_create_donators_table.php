6dac2ff930babb64872330294cbcc1c419417ea6
SerCycle <michelyanuar@gmail.com>
2022-01-03 15:49:07 +0700

    Update Migrate Donators

--- database/migrations/2022_01_03_072915_create_donators_table.php
+++ database/migrations/2022_01_03_072915_create_donators_table.php
@@ -14,7 +14,16 @@ class CreateDonatorsTable extends Migration
     public function up()
     {
         Schema::create('donators', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_donators');
+            $table->string('name_donators');
+            $table->string('email_donators')->unique();
+            $table->string('password_donators');
+            $table->string('blood_type_donators');
+            $table->string('blood_rhesus_donators');
+            $table->string('contact_donators');
+            $table->string('address_donators');
+            $table->string('point_donators');
+            $table->string('profile_images_donators')->nullable();
             $table->timestamps();
         });
     }
