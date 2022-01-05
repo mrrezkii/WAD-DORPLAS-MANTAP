24300c4af31241b365b6bb94cc342ccc7bb6f890
nando <muhammad.rezki.ananda@gmail.com>
2022-01-05 08:21:40 +0700

    Get article from api

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 
+use GuzzleHttp\Client;
+
 class ArticleController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $client = new Client;
+        $results = $client->request('GET', 'https://covid19.go.id/feed/berita');
+
+        $xml = simplexml_load_string($results->getBody(), 'SimpleXMLElement', LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $array = json_decode($json, true);
+        $collection = collect($array);
         return view('pages.article.index', [
             'title' => 'Artikel',
             'active' => 'article',
+            'datas' => $collection
         ]);
     }
 }
--- resources/views/pages/article/index.blade.php
+++ resources/views/pages/article/index.blade.php
@@ -5,99 +5,74 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
 @endsection
 @section('container')
+    {{--    @dd($datas['channel']['item'])--}}
     <div class="row">
         <div class="col-md-6 col-sm-12 mt-5">
             <div class="card mb-3">
-                <a href="#" class="text-decoration-none">
+                <a href="{{ $datas['channel']['item'][0]['link'] }}" target="_blank" class="text-decoration-none">
                     <img class="card-img-top"
-                         src="https://d324bm9stwnv8c.cloudfront.net/7-penyebab-nyeri-dada-sebelah-kiri-halodoc.jpg"
+                         src="{{ $datas['channel']['item'][0]['enclosure']['@attributes']['url'] }}"
                          alt="Card image cap">
                     <div class="card-body ml-3">
                         <h5 class="card-title text-title1 text-blue"
-                            style="font-size: 20px;font-family: 'Montserrat', sans-serif;">Cari tahu kenapa
-                            hati anda
-                            kosong ? Cek ulasan lengkapnya</h5>
+                            style="font-size: 20px;font-family: 'Montserrat', sans-serif;">{{ $datas['channel']['item'][0]['title'] }}</h5>
                         <div class="mt-3">
-                            <span class="text-body2 text-red" style="font-size: 18px;">Rezki</span>
+                            <span class="text-body2 text-red" style="font-size: 18px;">covid19.go.id</span>
                             <span class="text-body2 text-dark" style="font-size: 18px;">&bull;</span>
-                            <span class="text-body2 text-blue" style="font-size: 18px;">10 Okt 2021</span>
+                            <span class="text-body2 text-blue"
+                                  style="font-size: 18px;">{{ Str::replace('+0700', 'WIB', $datas['channel']['item'][0]['pubDate']) }}</span>
                         </div>
                     </div>
                 </a>
-
+            </div>
+            <div class="card mb-3">
+                <a href="{{ $datas['channel']['item'][1]['link'] }}" target="_blank" class="text-decoration-none">
+                    <img class="card-img-top"
+                         src="{{ $datas['channel']['item'][01]['enclosure']['@attributes']['url'] }}"
+                         alt="Card image cap">
+                    <div class="card-body ml-3">
+                        <h5 class="card-title text-title1 text-blue"
+                            style="font-size: 20px;font-family: 'Montserrat', sans-serif;">{{ $datas['channel']['item'][1]['title'] }}</h5>
+                        <div class="mt-3">
+                            <span class="text-body2 text-red" style="font-size: 18px;">covid19.go.id</span>
+                            <span class="text-body2 text-dark" style="font-size: 18px;">&bull;</span>
+                            <span class="text-body2 text-blue"
+                                  style="font-size: 18px;">{{ Str::replace('+0700', 'WIB', $datas['channel']['item'][1]['pubDate']) }}</span>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="col-md-6 col-sm-12 mt-5">
             <h6 class="mb-4 text-title1 text-blue">Artikel Pilihan</h6>
-            <a class="text-decoration-none" href="#">
-                <div class="row no-gutters mb-3 list-article">
-                    <div class="col-md-2 col-sm-12">
-                        <img class="rounded img-fluid"
-                             src="https://d324bm9stwnv8c.cloudfront.net/7-penyebab-nyeri-dada-sebelah-kiri-halodoc.jpg"
-                             width="80px" height="80px" alt="article">
-                    </div>
-                    <div class="col-md-10 col-sm-12">
-                        <p class="ml-2 p-0 text-title1 text-blue" style="max-lines: 1">Cari tahu kenapa
-                            hati anda
-                            kosong ? Cek ulasan lengkapnya</p>
-                        <p class="ml-2 text-body1 text-dark"
-                           style="max-lines: 1">Hal ini diungkapkan oleh ilmuwan
-                            terkenal Prof. Rere, bahwasannya hati kosong itu...</p>
-                        <p>
-                            <span class="ml-2 text-body2 text-red">Rezki</span>
-                            <span class="ml-2 text-body2 text-dark">&bull;</span>
-                            <span class="ml-2 text-body2 text-blue">10 Okt 2021</span>
-                        </p>
+            @foreach($datas['channel']['item'] as $data)
+                @if ($loop->first) @continue @endif
+                @if ($loop->index == 1) @continue @endif
+                <a href="{{ $data['link'] }}" target="_blank" class="text-decoration-none">
+                    <div class="row no-gutters mb-3 list-article">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="rounded img-fluid"
+                                 src="{{ $data['enclosure']['@attributes']['url'] }}"
+                                 alt="article">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <p class="ml-2 p-0 text-title1 text-blue" style="max-lines: 1">{{ $data['title'] }}</p>
+                            <p class="ml-2 text-body1 text-dark"
+                               style="max-lines: 1">Hal ini diungkapkan oleh ilmuwan
+                                terkenal Prof. Rere, bahwasannya hati kosong itu...</p>
+                            <p>
+                                <span class="ml-2 text-body2 text-red">covid19.go.id</span>
+                                <span class="ml-2 text-body2 text-dark">&bull;</span>
+                                <span
+                                    class="ml-2 text-body2 text-blue">{{ Str::replace('+0700', 'WIB', $data['pubDate']) }}</span>
+                            </p>
 
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a class="text-decoration-none" href="#">
-                <div class="row no-gutters mb-3 list-article">
-                    <div class="col-md-2 col-sm-12">
-                        <img class="rounded img-fluid"
-                             src="https://d324bm9stwnv8c.cloudfront.net/7-penyebab-nyeri-dada-sebelah-kiri-halodoc.jpg"
-                             width="80px" height="80px" alt="article">
-                    </div>
-                    <div class="col-md-10 col-sm-12">
-                        <p class="ml-2 p-0 text-title1 text-blue" style="max-lines: 1">Cari tahu kenapa
-                            hati anda
-                            kosong ? Cek ulasan lengkapnya</p>
-                        <p class="ml-2 text-body1 text-dark"
-                           style="max-lines: 1">Hal ini diungkapkan oleh ilmuwan
-                            terkenal Prof. Rere, bahwasannya hati kosong itu...</p>
-                        <p>
-                            <span class="ml-2 text-body2 text-red">Rezki</span>
-                            <span class="ml-2 text-body2 text-dark">&bull;</span>
-                            <span class="ml-2 text-body2 text-blue">10 Okt 2021</span>
-                        </p>
+                </a>
 
-                    </div>
-                </div>
-            </a>
-            <a class="text-decoration-none" href="#">
-                <div class="row no-gutters mb-3 list-article">
-                    <div class="col-md-2 col-sm-12">
-                        <img class="rounded img-fluid"
-                             src="https://d324bm9stwnv8c.cloudfront.net/7-penyebab-nyeri-dada-sebelah-kiri-halodoc.jpg"
-                             width="80px" height="80px" alt="article">
-                    </div>
-                    <div class="col-md-10 col-sm-12">
-                        <p class="ml-2 p-0 text-title1 text-blue" style="max-lines: 1">Cari tahu kenapa
-                            hati anda
-                            kosong ? Cek ulasan lengkapnya</p>
-                        <p class="ml-2 text-body1 text-dark"
-                           style="max-lines: 1">Hal ini diungkapkan oleh ilmuwan
-                            terkenal Prof. Rere, bahwasannya hati kosong itu...</p>
-                        <p>
-                            <span class="ml-2 text-body2 text-red">Rezki</span>
-                            <span class="ml-2 text-body2 text-dark">&bull;</span>
-                            <span class="ml-2 text-body2 text-blue">10 Okt 2021</span>
-                        </p>
+            @endforeach
 
-                    </div>
-                </div>
-            </a>
         </div>
     </div>
 @endsection
