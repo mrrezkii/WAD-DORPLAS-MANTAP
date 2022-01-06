8835e2e5769f87f1ff79b202c0be69e279d776b1
nando <muhammad.rezki.ananda@gmail.com>
2022-01-06 13:14:15 +0700

    Faq done

--- resources/views/pages/faq/index.blade.php
+++ resources/views/pages/faq/index.blade.php
@@ -20,14 +20,9 @@
                     <div id="collapseFaqOne" class="collapse show" aria-labelledby="faqOne" data-parent="#accordion">
                         <div class="card-body text-decoration-none">
                             <span><i class="fas fa-quote-right text-red"></i></span>
-                            <p class="text-title1" style="color: #0B090A">Anim pariatur cliche reprehenderit, enim
-                                eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                                cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                                wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't heard of them accusamus labore sustainable VHS.</p>
+                            <p class="text-title1" style="color: #0B090A">Dorplas adalah sebuah platform berbasis web &
+                                mobile yang memudahkan seseorang untuk melakukan donor plasma konvalesen yang langsung
+                                terhubung dengan Kantor Palang Merah Indonesia di setiap kota di Indonesia.</p>
 
                         </div>
                     </div>
@@ -39,7 +34,7 @@
                                     data-toggle="collapse" data-target="#collapseFaqTwo" aria-expanded="false"
                                     aria-controls="collapseFaqTwo">
                                 <span><i class="fas fa-question-circle text-red"></i></span>
-                                Apa itu Dorplas ?
+                                Apakah Dorplas adalah bagian dari Palang Merah Indonesia (PMI)? ?
                             </button>
                         </h5>
                     </div>
@@ -47,15 +42,11 @@
                     <div id="collapseFaqTwo" class="collapse" aria-labelledby="faqTwo" data-parent="#accordion">
                         <div class="card-body text-decoration-none">
                             <span><i class="fas fa-quote-right text-red"></i></span>
-                            <p class="text-title1" style="color: #0B090A">Anim pariatur cliche reprehenderit, enim
-                                eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                                cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                                wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't heard of them accusamus labore sustainable VHS.</p>
-
+                            <p class="text-title1" style="color: #0B090A">Iya. Dorplas bergerak dan berkolaborasi
+                                bersama Palang Merah Indonesia (PMI) untuk untuk membantu orang-orang yang membutuhkan
+                                plasma darah dalam proses pengobatan COVID-19 guna menurunkan lonjakan angka kematian
+                                akibat COVID-19.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -66,7 +57,7 @@
                                     data-toggle="collapse" data-target="#collapseFaqThree" aria-expanded="false"
                                     aria-controls="collapseFaqThree">
                                 <span><i class="fas fa-question-circle text-red"></i></span>
-                                Apa itu Dorplas ?
+                                Apakah donor plasma konvalesen itu menyakitkan?
                             </button>
                         </h5>
                     </div>
@@ -74,14 +65,12 @@
                     <div id="collapseFaqThree" class="collapse" aria-labelledby="faqThree" data-parent="#accordion">
                         <div class="card-body text-decoration-none">
                             <span><i class="fas fa-quote-right text-red"></i></span>
-                            <p class="text-title1" style="color: #0B090A">Anim pariatur cliche reprehenderit, enim
-                                eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                                cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                                wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't heard of them accusamus labore sustainable VHS.</p>
+                            <p class="text-title1" style="color: #0B090A">Sakit yang dirasakan hanya sesaat, seperti
+                                dicubit. Takut akan jarum menjadi faktor utama yang menyebabkan banyak orang berpikir
+                                bahwa proses donor plasma konvalesen akan menyakitkan. Sebagian besar calon pendonor
+                                merasa takut akan jarum, namun akhirnya merasakan proses donor plasma konvalesen tidak
+                                sesakit yang dipikirkan. Kamu dapat membaca beberapa tips yang dapat dilakukan untuk
+                                membantu proses donor plasma konvalesen pertama menjadi lebih nyaman.</p>
 
                         </div>
                     </div>
@@ -93,7 +82,7 @@
                                     data-toggle="collapse" data-target="#collapseFaqFour" aria-expanded="false"
                                     aria-controls="collapseFaqFour">
                                 <span><i class="fas fa-question-circle text-red"></i></span>
-                                Apa itu Dorplas ?
+                                Kenapa donor plasma konvalesen itu penting?
                             </button>
                         </h5>
                     </div>
@@ -101,14 +90,11 @@
                     <div id="collapseFaqFour" class="collapse" aria-labelledby="faqFour" data-parent="#accordion">
                         <div class="card-body text-decoration-none">
                             <span><i class="fas fa-quote-right text-red"></i></span>
-                            <p class="text-title1" style="color: #0B090A">Anim pariatur cliche reprehenderit, enim
-                                eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                                cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                                wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't heard of them accusamus labore sustainable VHS.</p>
+                            <p class="text-title1" style="color: #0B090A">Sesuai dengan slogan dari PMI: “Mereka
+                                selamat, kita sehat”. Donor plasma konvalesen dapat menyelamatkan nyawa pasien yang
+                                membutuhkan transfusi plasma untuk alternatif pengobat covid. Selain menyelamatkan nyawa
+                                pasien, donor plasma konvalesen juga menyehatkan pendonornya, karena membantu regenerasi
+                                sel darah baru sehingga mengurangi risiko serangan jantung, kanker dan sebagainya.</p>
 
                         </div>
                     </div>
@@ -120,7 +106,7 @@
                                     data-toggle="collapse" data-target="#collapseFaqFive" aria-expanded="false"
                                     aria-controls="collapseFaqFive">
                                 <span><i class="fas fa-question-circle text-red"></i></span>
-                                Apa itu Dorplas ?
+                                Siapa dibalik Dorplas ?
                             </button>
                         </h5>
                     </div>
@@ -128,14 +114,20 @@
                     <div id="collapseFaqFive" class="collapse" aria-labelledby="faqFive" data-parent="#accordion">
                         <div class="card-body text-decoration-none">
                             <span><i class="fas fa-quote-right text-red"></i></span>
-                            <p class="text-title1" style="color: #0B090A">Anim pariatur cliche reprehenderit, enim
-                                eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                                cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                                wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't heard of them accusamus labore sustainable VHS.</p>
+                            <p class="text-title1 text-grey">
+                                <span style="color: #903749">Tim Website :</span>
+                                <br>
+                                - Alif Yanuar Aditya Subagyo <br>
+                                - Bagus Tri Atmaja <br>
+                                - Muhammad Ihsan Izzulhaq <br>
+                                - Muhammad Rezki Ananda <br>
+                                - Yanuar Taruna Lutfi <br>
+                                <br>
+                                <br>
+                                <span style="color: #903749">Tim Mobile :</span>
+                                <br>
+                                - Muhammad Rezki Ananda (Squad Soematera - GITS)
+                            </p>
 
                         </div>
                     </div>
