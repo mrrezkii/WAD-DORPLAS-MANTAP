c45db61ffaa6d0ef108a3b5d1307a4e45eaf3f97
hanstl <77946714+hanstl@users.noreply.github.com>
2022-01-08 12:53:54 +0700

    DonorSubmissionEmployeeController update 8 Jan

--- app/Http/Controllers/DonorSubmissionController.php
+++ app/Http/Controllers/DonorSubmissionController.php
@@ -2,67 +2,52 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Models\DonorSubmissions;
-use App\Models\Institutions;
 use Illuminate\Http\Request;
-use Ramsey\Uuid\Uuid;
 
-class DonorSubmissionController extends Controller
+class DonorSubmissionEmployeeController extends Controller
 {
+
     public function index()
     {
-        return view('pages.donor.submission', [
+        return view('pages.donor.submission_admin', [
             'title' => 'Request Plasma',
-            'active' => 'donor',
-            'stockPlasma' => $this->dashboard()->stockPlasma(),
-            'totalRequest' => $this->dashboard()->requestPlasma(),
-            'institutions' => Institutions::all()
+            'active' => 'submission',
         ]);
     }
 
-    public function dashboard()
+
+    public function create()
     {
-        return new DashboardController();
+        //
     }
+
 
     public function store(Request $request)
     {
-
-        $validateData = $request->validate([
-            'recipient_donor_submissions' => 'required|max:255',
-            'applicant_donor_submissions' => 'required|max:255',
-            'blood_type_donor_submissions' => 'required|max:2',
-            'rhesus_type_donor_submissions' => 'required|max:255',
-            'quantity_donor_submissions' => 'required|max:255',
-            'id_institutions' => 'required|max:255',
-            'ktp_donor_submissions' => 'required|mimes:jpeg,png,jpg,gif,svg,pdf',
-            'letter_donor_submissions' => 'required|mimes:jpeg,png,jpg,gif,svg,pdf',
-        ]);
-
-        $ktpFile = $request->file('ktp_donor_submissions');
-        $letterFile = $request->file('letter_donor_submissions');
-
-        $uuidShorten = str_replace('-', '', $request->id_donators);
-        $ktpName = time() . "_" . $uuidShorten . "_" . $ktpFile->getClientOriginalName();
-        $letterName = time() . "_" . $uuidShorten . "_" . $letterFile->getClientOriginalName();
-
-        $ktpPath = "upload/$uuidShorten/ktp";
-        $letterPath = "upload/$uuidShorten/letter";
-
-        $ktpFile->move($ktpPath, $ktpName);
-        $letterFile->move($letterPath, $letterName);
-
-        $validateData['id_donor_submissions'] = Uuid::uuid4()->toString() . "\n";
-        $validateData['id_donators'] = $request->id_donators;
-        $validateData['status_donor_submissions'] = 1;
-        $validateData['quantity_donor_submissions'] = (int)$request->quantity_donor_submissions;
-        $validateData['ktp_donor_submissions'] = "/$ktpPath/$ktpName";
-        $validateData['letter_donor_submissions'] = "/$letterPath/$letterName";
-
-        DonorSubmissions::create($validateData);
+        //
+    }
 
 
-        return redirect('/submission')->with('submissionSuccess', 'Berhasil Request Plasma');
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
     }
 }
