4ff1c74ed69e414dc7b28eb17f2563ebd0213d9e
hanstl <77946714+hanstl@users.noreply.github.com>
2022-01-08 13:22:11 +0700

    add update password feature

--- app/Http/Controllers/AccountEmployeeController.php
+++ app/Http/Controllers/AccountEmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Employees;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AccountEmployeeController extends Controller
 {
@@ -36,7 +37,7 @@ class AccountEmployeeController extends Controller
     {
         $idEmployees = Auth::check() ? Auth::id() : true;
         $validateData = $request->validate([
-            'email_employees' => 'required|max:255|email:dns|unique:donators,email_donators,NULL,id_donators',
+            'email_employees' => 'required|max:255|email:dns|unique:employees,email_employees,NULL,id_employees',
             'address_employees' => 'required',
             'contact_employees' => 'required|max:20',
         ]);
@@ -44,6 +45,26 @@ class AccountEmployeeController extends Controller
         Employees::where('id_employees', '=', $idEmployees)->update($validateData);
 
         return redirect('/_account')->with('updateSuccess', 'Berhasil Memperbarui Kontak');
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $user = Auth::guard('employee')->user();
+        $userPassword = $user->password_employees;
+
+        if (!Hash::check($request->old_password_employees, $userPassword)) {
+            return back()->withErrors(['old_password_employees' => 'Password not match']);
+        }
+
+        $request->validate([
+            'old_password_employees' => 'required|min:5|max:255',
+            'new_password_employees' => 'required|min:5|max:255'
+        ]);
+
+        $user->password_employees = Hash::make($request->new_password_employees);
+        $user->save();
+
+        return redirect('/_account')->with('updateSuccess', 'Berhasil Memperbarui Password');
     }
 
 
