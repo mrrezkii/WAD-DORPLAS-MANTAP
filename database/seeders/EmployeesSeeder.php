db46164e50d55294ce6baa4a3eeb8e6d56a9e548
Muhammad Ihsan Izzulhaq <mi.izzulhaq@gmail.com>
2022-01-09 12:39:26 +0700

    Update Employees Seeder

--- database/seeders/EmployeesSeeder.php
+++ database/seeders/EmployeesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Employees;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
 
 class EmployeesSeeder extends Seeder
 {
@@ -15,5 +17,45 @@ class EmployeesSeeder extends Seeder
     public function run()
     {
         Employees::factory(5)->create();
+
+        Employees::create([
+            'id_employees' => Uuid::uuid4()->toString() . "\n",
+            'id_institutions' => '731ef6df-6171-33fd-bd05-93cd76db2cdd',
+            'name_employees' => 'Muhammad Rezki Ananda',
+            'email_employees' => 'muhammad.rezki.ananda@pmi.co.id',
+            'password_employees' => Hash::make(12345),
+            'contact_employees' => '085608845319',
+            'address_employees' => 'Jl.Kediri Raya Weeh, Jawa TImur'
+            ]);
+
+        Employees::create([
+            'id_employees' => Uuid::uuid4()->toString() . "\n",
+            'id_institutions' => '272f8cd3-f856-37f0-ac40-94c3b36d52da',
+            'name_employees' => 'Alif Yanuar Aditya Subagyo',
+            'email_employees' => 'michelyanuar@pmi.co.id',
+            'password_employees' => Hash::make(12345),
+            'contact_employees' => '087772934259',
+            'address_employees' => 'Jln. Isekai, mantep mantep aselole jos jos jos'
+        ]);
+
+        Employees::create([
+            'id_employees' => Uuid::uuid4()->toString() . "\n",
+            'id_institutions' => '9d25e4f1-fbaf-359e-9e45-890677a67c0d',
+            'name_employees' => 'Bagus Tri Atmaja',
+            'email_employees' => 'bagustri734@pmi.co.id',
+            'password_employees' => Hash::make(12345),
+            'contact_employees' => '082145198847',
+            'address_employees' => 'Jl bukan jokowi no 69 kyoto, jepang'
+        ]);
+
+        Employees::create([
+            'id_employees' => Uuid::uuid4()->toString() . "\n",
+            'id_institutions' => '0a2fdb17-2d3e-3949-9f36-68cf14b941e5',
+            'name_employees' => 'Muhammad Ihsan Izzulhaq',
+            'email_employees' => 'mi.izzulhaq@pmi.co.id',
+            'password_employees' => Hash::make(12345),
+            'contact_employees' => '081286226512',
+            'address_employees' => 'Jl. Aster, Kab. Tokyo - Japan'
+        ]);
     }
 }
